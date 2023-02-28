3562229c1e21ff58ac66d2dfb6700b2356084da0
JOYBOY-0 <falamino00@gmail.com>
2023-02-28 19:49:41 -0300

    add confirmation code component

---
+++ src/views/confirmation/components/codeInput/index.tsx
@@ -0,0 +1,73 @@
+import { ArrowPathIcon } from '@heroicons/react/24/solid';
+import React, { useEffect, useRef, useState } from 'react';
+
+interface Props {
+  onConfirm: (code: string) => void;
+}
+
+const ConfirmationCodeInput: React.FC<Props> = ({ onConfirm }) => {
+  const inputs = useRef<HTMLInputElement[]>([]);
+  const [values, setValues] = useState<string[]>(new Array(6).fill(''));
+
+  const handleChange = (index: number, value: string) => {
+    const newValues = [...values];
+    newValues[index] = value;
+    setValues(newValues);
+
+    if (index < inputs.current.length - 1 && value) {
+      inputs.current[index + 1]?.focus();
+    }
+  };
+
+  const handlePaste = (event: React.ClipboardEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const clipboardData = event.clipboardData.getData('text/plain').slice(0, 6);
+    const newValues = [...values];
+    for (let i = 0; i < clipboardData.length && i < inputs.current.length; i++) {
+      const digit = clipboardData[i];
+      newValues[i] = digit;
+    }
+    setValues(newValues);
+    inputs.current[0]?.focus();
+  };
+
+  const handleReset = () => {
+    setValues(new Array(6).fill(''));
+    inputs.current[0]?.focus();
+  };
+
+  useEffect(() => {
+    if (values.every((value) => value !== '') && values.length === 6) {
+      handleSubmit();
+    }
+  }, [values]);
+
+  const handleSubmit = () => {
+    const code = values.join('');
+    onConfirm(code);
+  };
+
+  return (
+    <div className='flex gap-2'>
+      {values.map((value, index) => (
+        <input
+          className="w-10 h-10 rounded border-2 border-gray-300 text-lg leading-none"
+          key={index}
+          type="text"
+          maxLength={1}
+          value={value}
+          onChange={(event) => handleChange(index, event.target.value)}
+          onPaste={handlePaste}
+          ref={(el) => (inputs.current[index] = el!)}
+        />
+      ))}
+      <div className='flex flex-col items-center justify-center ml-2'>
+        <ArrowPathIcon className='text-gray-500 h-6 w-6' />
+        <button className='text-sm text-gray-500' onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationCodeInput;
+
