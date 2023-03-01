b6437b0e75a878b64357d1a8e062afb6f781ea20
JOYBOY-0 <falamino00@gmail.com>
2023-02-28 21:02:03 -0300

    convert actions to links

--- src/views/home/components/actionsSection/index.tsx
+++ src/views/home/components/actionsSection/index.tsx
@@ -1,17 +1,19 @@
 import { ArrowsRightLeftIcon, BanknotesIcon, Cog6ToothIcon, GiftIcon } from '@heroicons/react/24/solid'
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
 
 interface ActionItemProps {
     title: string
     icon: React.ReactNode
+    to: string
 }
 
-const ActionItem : FC<ActionItemProps> = ({ title, icon }) => {
+const ActionItem : FC<ActionItemProps> = ({ title, icon, to }) => {
     return (
-        <div className='flex flex-col items-center justify-center rounded-lg w-32 gap-2'>
+        <Link to={to} className='flex flex-col items-center justify-center rounded-lg w-32 gap-2'>
             {icon}
             <p className='text-xs text-orange-500 text-center whitespace-nowrap'>{title}</p>
-        </div>
+        </Link>
     )
 }
 
@@ -20,19 +22,23 @@ export const ActionsSection = () => {
     let actions = [
         {
             title: 'Cargar saldo',
-            icon: <BanknotesIcon className={iconStyle} />
+            icon: <BanknotesIcon className={iconStyle} />,
+            to: '/add'
         },
         {
             title: 'Enviar premio',
-            icon: <GiftIcon className={iconStyle} />
+            icon: <GiftIcon className={iconStyle} />,
+            to: '/'
         },
         {
             title: 'Movimientos',
-            icon: <ArrowsRightLeftIcon className={iconStyle} />
+            icon: <ArrowsRightLeftIcon className={iconStyle} />,
+            to: '/'
         },
         {
             title: 'Ajustes',
-            icon: <Cog6ToothIcon className={iconStyle} />
+            icon: <Cog6ToothIcon className={iconStyle} />,
+            to: '/'
         }
     ]
 
@@ -41,7 +47,7 @@ export const ActionsSection = () => {
         <h2 className='text-xl text-gray-800 font-bold'>Acciones</h2>
         <div className='flex items-start gap-2 my-4'>
             {actions.map((action, index) => (
-                <ActionItem key={index} title={action.title} icon={action.icon} />
+                <ActionItem key={index} title={action.title} icon={action.icon} to={action.to} />
             ))}
         </div>
     </section>
