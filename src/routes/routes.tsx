bb42dddb62731b9346f3c21b32a4d3f49e44a45a
JOYBOY-0 <falamino00@gmail.com>
2023-02-28 21:59:17 -0300

    add founds view

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,25 +3,30 @@ import {RouteObject} from 'react-router'
 import { Confirmation } from "../views/confirmation";
 import AuthGuard from "./guard";
 import { Home } from "../views/home";
+import { AddFounds } from "../views/addFounds";
 
 export const PRIVATE: Record<string, RouteObject> = {
-    HOME: {
-      path: '/home',
-      element: <Home />,
-    },
+  HOME: {
+    path: '/home',
+    element: <Home />,
+  },
+  ADD: {
+    path: '/add',
+    element: <AddFounds />,
+  },
 }
 
 export const ROUTES: Record<string, RouteObject> = {
-    AUTH: {
-      path: '/',
-      element: <Auth />,
-    },
-    CONFIRMATION: {
-      path: '/confirmation',
-      element: <Confirmation />,
-    },
-    PRIVATE: {
-        element: <AuthGuard />,
-        children: Object.values(PRIVATE),
-    }
+  AUTH: {
+    path: '/',
+    element: <Auth />,
+  },
+  CONFIRMATION: {
+    path: '/confirmation',
+    element: <Confirmation />,
+  },
+  PRIVATE: {
+      element: <AuthGuard />,
+      children: Object.values(PRIVATE),
+  }
 }
